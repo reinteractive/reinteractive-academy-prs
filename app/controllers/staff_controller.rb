48f16c7e7a47033ffa78da411323ae1b4240ece6
nicholasg <350zaina@gmail.com>
2019-07-04 11:37:56 +1000

    staff feature has draft update form & premigrated last_name attribute

--- app/controllers/staff_controller.rb
+++ app/controllers/staff_controller.rb
@@ -1,10 +1,37 @@
 class StaffController < ApplicationController
   def index
+    @staffs = Staff.all
   end
 
   def new
   end
 
   def edit
+    @staff = Staff.find(params[:id])
+  end
+
+
+
+    def update
+      @staff = Staff.find(staff_params)
+        if @staff.update_attributes(params[:staff])
+          flash[:success] = "Staff was successfully updated"
+          redirect_to @staff
+        else
+          flash[:error] = "Something went wrong"
+          render 'edit'
+        end
+    end
+  
+  def show
+    # @staff = Staff.find(params[:id])
+  end
+
+
+
+  private
+
+  def staff_params
+    params.require(:staff).permit(:name)
   end
 end
