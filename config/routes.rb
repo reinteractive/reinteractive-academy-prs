98d5e77fe900c05e02acf794ddcb5f98254950c2
nicholasg <350zaina@gmail.com>
2019-07-15 10:01:20 +1000

    moded routes added meta-requesst gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,31 @@
 Rails.application.routes.draw do
-  resources :category,  :only => [:index, :new]
-  resources :customer,  :only => [:index, :edit, :new]
-  resources :dashboard, :only => [:index]
-  resources :ideas,     :only => [:index, :edit, :new]
-  resources :feedback,  :only => [:index, :show, :new]
-  resources :portal,    :only => [:index]
-  resources :staffs,    :only => [:create, :index, :new, :edit, :update]
-  resources :status,    :only => [:edit]
+  # resources :category,  :only => [:index, :new]
+  # resources :customer,  :only => [:index, :edit, :new]
+  # resources :dashboard, :only => [:index]
+  # resources :ideas,     :only => [:index, :edit, :new]
+  # resources :feedback,  :only => [:index, :show, :new]
+  # resources :portal,    :only => [:index]
+  # resources :staffs,    :only => [:create, :index, :new, :edit, :update]
+  # resources :status,    :only => [:edit]
+  
+  # scope namespace: :staffs do
+  #   resources :ideas
+  #   resources :feedback
+  #   # /ideas => app/controllers/staff/ideas_controller.rb
+  #   # /feedback => app/controllers/staff/feedbacks_controller.rb
+  # end
 
+  # scope module: 'admin' do
+  #   resources :staffs
+  #   # controller: app/controlers/admin/articles_controller.rb
+  #   # url: /staffs
+  # end
+
+  # resources :verifications, path: '/verify', as: :foo  
+
+  # get "/hello", to: "foo#bar"
+
+  
   root "portal#index"
 end
 
