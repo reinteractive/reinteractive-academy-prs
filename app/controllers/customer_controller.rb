6ab384ad701df70c414f22b86d2f067181983f4b
nicholasg <350zaina@gmail.com>
2019-07-16 11:46:43 +1000

    premigrated customer with draft controller & view

--- app/controllers/customer_controller.rb
+++ app/controllers/customer_controller.rb
@@ -3,8 +3,31 @@ class CustomerController < ApplicationController
   end
 
   def new
+    @customer = Customer.new
   end
+
+   def create
+     @customer = Customer.new(@customer_params)
+       if @customer.save
+         redirect_to variables_path
+       else
+         render 'new'
+       end
+   end
+  
 
   def edit
   end
+
+
+
+
+  private
+
+   def customer_params
+     params.require(:customer).permit(:name)
+   end
+  
 end
+
+ 
