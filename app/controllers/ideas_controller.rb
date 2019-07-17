542d921f855cceb96447dd91ba9dfd2bfa3096c2
nicholasg <350zaina@gmail.com>
2019-07-17 13:13:12 +1000

    premigrated Idea object

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,10 +1,43 @@
 class IdeasController < ApplicationController
   def index
+    @ideas = Idea.all
   end
 
   def new
+    @idea = Idea.new
   end
 
-  def edit
+def create
+  @idea = Idea.new(idea_params)
+  if @idea.save
+    redirect_to action: "index"
+  else
+    render 'new'
   end
 end
+
+
+  def edit
+    @idea = Idea.find(params[:id])
+  end
+
+  def update
+     @idea = Idea.find(params[:id])
+    if @idea.update_attributes(idea_params)
+      redirect_to ideas_path
+    else
+      render action: 'edit'
+    end
+  end
+
+  def show
+    # @idea = idea.find(params[:id])
+  end
+  private
+
+  def idea_params
+    params.require(:idea).permit(:name, :last_name)
+  end
+end
+
+
