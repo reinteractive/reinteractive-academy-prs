06ce5a51ffb92b8070240167c7c660b11923f076
nicholasg <350zaina@gmail.com>
2019-07-23 11:28:18 +1000

    mmigration being funky so deleted previous migration & added new one

--- db/migrate/20190719010521_add_idea_id_to_staff.rb
+++
@@ -1,5 +0,0 @@
-class AddIdeaIdToStaff < ActiveRecord::Migration[5.2]
-  def change
-    add_column :staffs, :idea_id, :integer
-  end
-end
---
+++ db/migrate/20190723012714_add_staf_f_id_to_idea.rb
@@ -0,0 +1,5 @@
+class AddStafFIdToIdea < ActiveRecord::Migration[5.2]
+  def change
+    add_column :ideas, :staff_id, :integer
+  end
+end
