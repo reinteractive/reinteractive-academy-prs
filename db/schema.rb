7de5e01b05be8fa75733d8370f9e45889f0e97c2
nicholasg <350zaina@gmail.com>
2019-07-28 13:30:09 +1000

    post migrated category table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_27_025646) do
+ActiveRecord::Schema.define(version: 2019_07_28_032551) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string "name"
