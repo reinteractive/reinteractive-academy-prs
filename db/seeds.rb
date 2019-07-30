12a054ac6e1e77e74a490d241f6ba7467f45d872
nicholasg <350zaina@gmail.com>
2019-07-30 13:17:11 +1000

    moded seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,28 @@
 require 'faker'
 
 Staff.delete_all
-Customer.delete_all
-Idea.delete_all
-Status.delete_all
+# Customer.delete_all
+# Status.delete_all
+# Category.delete_all
+# Idea.delete_all
+
+
+2.times do
+  category = Category.create!(name: Faker::Lorem.words(4))
+end
+
+
+                              
 
 2.times do
   customer = Customer.create!(name: Faker::Name.first_name,
                               feedback: Faker::Company.catch_phrase)
+end 
+
+
 2.times do
   idea = Idea.create!(title: Faker::Company.catch_phrase,
-                      customer_id: customer.id)
+                      customer_id: :customer_id)
                                 # email: Faker::Internet.free_email,
                                 # password: 'hello123',
                                 # bio: Faker::Company.catch_phrase,
@@ -22,25 +34,25 @@ Status.delete_all
                                 # invitations_count: (rand(9)+1).to_i,
                                 # invitation_sent_at: Faker::Date.between(200.days.ago, Date.today),
                                 # invitation_accepted_at: Faker::Date.between(200.days.ago, Date.today))
+end
 
 2.times do
   staff = Staff.create!(name: Faker::Name.first_name,
-                        idea_id: idea.id)
-5.times do |n|
-  Status.create(title: "Idea ##{n}", status_id: random_status)
+                        idea_id: :idea_id)
+end
 
-
+Status.create!(stage: :planned)
+Status.create!(stage: :release)
+# Status.create!(stage: :awaiting_feedback)  
+# Status.create!(stage: :under_review)
+Status.create!(stage: :building)
+ 
 def random_status
   Status.order('RANDOM()').first
 end
 
-5.times do |n|
-  Idea.create(title: "Idea ##{n}", status_id: random_status)
-
-
-        end          
-     end
-   end
-end
+# 5.times do |n|
+#   Idea.create(title: "Idea ##{n}", status_id: random_status)
+# end
 
 
