5218549834dc5ef73af29b2ca3c0e69f6a1788f6
nicholasg <350zaina@gmail.com>
2019-08-28 10:48:20 +1000

    draft 3 categories completed

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,9 @@
-class CategoriesController < ApplicationController
+app/views/categories/index.html.erbclass CategoriesController < ApplicationController
   def index
     @categories = Category.all
+    @ideas = Idea.all
+   
+     
   end
 
   def new
@@ -8,37 +11,32 @@ class CategoriesController < ApplicationController
   end
 
 
-   def create
-     @category = Category.new(category_params)
-       if @category.save
-         redirect_to action: 'index'
-       else
-         render 'new'
-       end
-   end
-
+  def create
+    @category = Category.new(category_params)
+      if @category.save
+        redirect_to action: 'index'
+      else
+        render 'new'
+      end
+  end
 
   def edit
     @category = Category.find(params[:id])
   end
 
-    def update
-       @category = Staff.find(params[:id])
-      if @category.update_attributes(categories_params)
-        redirect_to categories_path
-      else
-        render action: 'edit'
-      end
+  def update
+    @category = Staff.find(params[:id])
+    if @category.update_attributes(categories_params)
+      redirect_to categories_path
+    else
+      render action: 'edit'
     end
-
-
-
+  end
 
 private
 
-   def category_params
-     params.require(:category).permit(:name)
-   end
+  def category_params
+    params.require(:category).permit(:name)
+  end
 
-  
 end
