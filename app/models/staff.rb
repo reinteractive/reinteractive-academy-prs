32099f15b72e9b4053e5a4958de2293f4b718096
nicholasg <350zaina@gmail.com>
2019-08-28 10:50:01 +1000

    draft 3 staff completed

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,7 +1,7 @@
 class Staff < ApplicationRecord
   has_many :ideas
   # has_many :statuses
-  belongs_to :status, required: false 
+  belongs_to :status, required: false
   
 
 accepts_nested_attributes_for :status
