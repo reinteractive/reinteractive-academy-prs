058aeb0cc6fdf7b5954c340347dfea3cf5b8dfa2
nicholasg <350zaina@gmail.com>
2019-08-28 10:49:16 +1000

    draft 3 status completed

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -4,22 +4,23 @@ class StatusController < ApplicationController
    @staff = Staff.find(params[:id])
    @idea = Idea.find(params[:id])
   end
+  
   def update
-      @status = Status.find(params[:id])
-      @staff = Staff.find(params[:id])
-      @idea = Idea.find(params[:id]))
-       if @status.update_attributes(status_params)
-          redirect_to ideas_path
-        else
-          render 'edit'
-      end
+    @status = Status.find(params[:id])
+    @staff = Staff.find(params[:id])
+    @idea = Idea.find(params[:id])
+     if @status.update_attributes(status_params)
+        redirect_to ideas_path
+      else
+        render 'edit'
     end
+  end
 
 
-    private
+  private
     
-    def status_params
-      params.require(:status).permit(:stage, :message, :name, :status_id, :title, :description )
-    end
+  def status_params
+    params.require(:status).permit(:stage, :message, :name, :status_id, :title, :description )
+  end
 
 end
