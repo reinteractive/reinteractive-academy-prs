87d9a6bc4f10dfc46fd18a1893d083783549c085
nicholasg <350zaina@gmail.com>
2019-09-03 09:25:14 +1000

    commiting seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,93 +12,69 @@ Feedback.delete_all
 AssignedFeedback.delete_all
 puts 'Cleanup done!'
 
- puts 'Adding categories...'
- Category.create!(name: 'Feature Requests')
- Category.create!(name: 'Bugs')
- puts 'Categories done!'
+puts 'Adding categories...'
+Category.create!(name: 'Feature Requests')
+Category.create!(name: 'Bugs')
+puts 'Categories done!'
 
- puts 'Adding statuses...'
- Status.create!(stage: 'Planned')
- Status.create!(stage: 'Released')
- Status.create!(stage: 'Awaiting Feedback')
- Status.create!(stage: 'Building')
- Status.create!(stage: 'Under Review')
- Status.create!(stage: 'Released')
- puts 'Statuses done!'
-
-puts 'Adding Customers...'
-5.times do |customer|
-  customer = Customer.create(
-  name: Faker::Name.first_name,
-  feedback: Faker::GreekPhilosophers.quote,
+puts 'Adding statuses...'
+Status.create!(stage: 'Planned')
+Status.create!(stage: 'Released')
+Status.create!(stage: 'Awaiting Feedback')
+Status.create!(stage: 'Building')
+Status.create!(stage: 'Under Review')
+Status.create!(stage: 'Released')
+puts 'Statuses done!'
   
-  email: Faker::Internet.free_email)
- end
-  puts 'Customers Done'
+puts 'Adding ideas...'
+5.times do |number|
+ idea = Idea.create(
+   title: "Item ##{number}",
+   description: Faker::Lorem.paragraph)
 
+  puts 'Adding feedback to idea...'
+  3.times do
+    idea.feedbacks.create!(
+      description: Faker::Lorem.paragraph,
+      customer_id: Customer.order('RANDOM()').first)
+  end
+  puts 'Feedback to ideas done!'
 
-puts 'Adding Feedback...'
-5.times do |feedback|
-  feedback = Feedback.create!(description: Faker::Company.catch_phrase)
-  puts 'Adding feedbacks to idea...'
-  feedback.ideas.create!(description: Idea.order('RANDOM()').first)
-  puts 'Feedbacks to ideas done!'
-  puts 'Adding feedback to customer...' 
-  feedback.customer = Customer.order('RANDOM()').first
-  feedback.save
-  puts 'Feedback to customer done!'
   
-end
-  
+  # puts 'Adding staffs to idea...'
+  # 3.times do
+  #   idea.staffs.create!(name: Faker::Name.first_name,
+  #       first_name: Faker::Name.first_name,
+  #       last_name: Faker::Name.name,
+  #       email: Faker::Internet.free_email,
+  #       idea_id:            )
+  #   end
+  puts 'Customers to idea done...'
 
 
 
- puts 'Adding ideas...'
- 5.times do |number|
-   idea = Idea.create(
-     title: "Item ##{number}",
-     description: Faker::Company.catch_phrase
-   )
-   puts 'Adding customers to idea...'
-   3.times do
-     idea.customers.create!(name: Customer.order('RANDOM()').first)
-   end
-   puts 'Customers done!'
+  puts 'Adding categories to idea...'
+  3.times do
+    idea.categories.create!(name: Faker::Name.first_name)
+  end
+  puts 'Categories to idea done!'
 
-    puts 'Adding categores to idea...'
-    3.times do
-      idea.categories.create!(name: Faker::Name.first_name)
-    end
-    puts 'Categories to idea done!'
-
-    puts 'Adding statuses to idea...'
+  puts 'Adding statuses to idea...'
+  3.times do
     idea.idea_statuses.create!(status: Status.order('RANDOM()').first)
-    puts 'Statuses done!'
-end
-  puts 'Ideas done!'
+    puts 'statuses to idea done!'
+  end
+   
 
-  puts 'Adding staff...'
-
+  puts 'Adding Staff...'
   5.times do |staff|
     staff = Staff.create!(name: Faker::Name.first_name,
                           first_name: Faker::Name.first_name,
                           email: Faker::Internet.free_email)
   end
   puts 'Staff done!'
+end
 
 
- 
-
-  # puts 'Adding feedbacks to customer...'
-  #     feedback.customer.create!(name: Customer.order('RANDOM()'), customer_id: customer.id)
-  #   puts 'Feedbacks to customers done!'
-
-
-  # customer.each_with_index do |obj, index|
-  #   Feedback.create!([{ feedback_id: Customer.order('RANDOM()'), :customer_id => obj.id }])
-  # end
-
-
-  
 
 
